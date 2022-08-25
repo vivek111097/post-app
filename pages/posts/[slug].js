5f3aeaeafbe31f8ba7e69308f0d9225a14be4e12
Vivek Chaudhari <vivek_chaudhari@ildc-inlp.idealake.com>
2022-08-25 13:37:34 +0530

    proc file updated

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,35 +1,11 @@
 import React from 'react'
-import FeaturedPosts from '../../components/home-page/featured-posts'
-import Hero from '../../components/home-page/hero'
-
-const DUMMY_POSTS = [
-    {
-      title: "Tiger",
-      image: "tiger.png",
-      excerpt: "Next is the React Framework",
-      date:"1997-10-11",
-      slug: "getting-started-with-nextjs",
-    },
-    {
-      title: "Tiger",
-      image: "tiger.png",
-      excerpt: "Next is the React Framework",
-      date:"1997-10-11",
-      slug: "getting-started-with-nextjs",
-    },
-    {
-      title: "Tiger",
-      image: "tiger.png",
-      excerpt: "Next is the React Framework",
-      date:"1997-10-11",
-      slug: "getting-started-with-nextjs",
-    },
-  ];
+import PostContent from './post-detail/post-content'
 
 const PostDetailPage = (props) => {
     return (
         <>
         <h1>slug</h1>
+        <PostContent/>
         </>
     )
 }
---
+++ pages/posts/post-detail/post-content.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import classes from './post-content.module.css'
+import PostHeader from './post-header'
+import ReactMarkdown from 'react-markdown'
+const DUMMY_POSTS =
+{
+    title: "Tiger",
+    image: "tiger.png",
+    date: "1997-10-11",
+    slug: "getting-started-with-nextjs-1",
+    content: '# This is a first post'
+}
+
+
+const PostContent = () => {
+    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+    return (
+        <article className={classes.content}>
+            <PostHeader title={DUMMY_POSTS.image} image={imagePath} />
+
+            <ReactMarkdown>{
+                DUMMY_POSTS.content
+            }</ReactMarkdown>
+        </article>
+    )
+}
+
+export default PostContent
